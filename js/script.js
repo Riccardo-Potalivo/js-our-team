91019241a89d9ce944377840d9fdb244fa7128b6
Riccardo Potalivo <riccardopotalivo@gmail.com>
2023-10-23 15:29:15 +0200

    milestone-2-done

--- js/script.js
+++ js/script.js
@@ -9,32 +9,32 @@ function myScript()
     [
         {
             name: 'Wayne Barnett',
-            Role: 'Founder & CEO',
+            role: 'Founder & CEO',
             picture: 'wayne-barnett-founder-ceo.jpg'
         },
         {
             name: 'Angela Caroll',
-            Role: 'Chief Editor',
+            role: 'Chief Editor',
             picture: 'angela-caroll-chief-editor.jpg'
         },
         {
             name: 'Walter Gordon',
-            Role: 'Office Manager',
+            role: 'Office Manager',
             picture: 'walter-gordon-office-manager.jpg'
         },
         {
             name: 'Angela Lopez',
-            Role: 'Social Media Manager',
+            role: 'Social Media Manager',
             picture: 'angela-lopez-social-media-manager.jpg'
         },
         {
             name: 'Scott Estrada',
-            Role: 'Developer',
+            role: 'Developer',
             picture: 'scott-estrada-developer.jpg'
         },
         {
             name: 'Barbara Ramos',
-            Role: 'Graphic Designer',
+            role: 'Graphic Designer',
             picture: 'barbara-ramos-graphic-designer.jpg'
         }
     ]
@@ -43,14 +43,14 @@ function myScript()
     {
         console.group();
         console.log("Name: " + ourTeam[i].name);
-        console.log("Role: " + ourTeam[i].Role);
+        console.log("Role: " + ourTeam[i].role);
         console.log("Picture: " + ourTeam[i].picture);
         console.groupEnd();
 
-        card()
+        card(ourTeam[i])
     }
 
-    function card()
+    function card(ourTeam)
     {
         // creo il contenitore della card
         const col = document.createElement('div');
@@ -60,10 +60,10 @@ function myScript()
         const cardLayout =
         `
         <div class="card text-center">
-            <img src="img/angela-caroll-chief-editor.jpg" class="card-img-top" alt="...">
+            <img src="img/${ourTeam.picture}" class="card-img-top" alt="...">
             <div class="card-body">
-                <h6 class="card-title">Name</h6>
-                <p class="card-text">Role</p>
+                <h6 class="card-title">${ourTeam.name}</h6>
+                <p class="card-text">${ourTeam.role}</p>
             </div>
         </div>
         `
