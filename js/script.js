5043397e8f40a40076655617f05a6fccb48100e2
Riccardo Potalivo <riccardopotalivo@gmail.com>
2023-10-23 13:49:38 +0200

    milestone 0 done

--- js/script.js
+++ js/script.js
@@ -3,6 +3,40 @@
 myScript();
 function myScript()
 {
+    // array dati team
+    const ourTeam =
+    [
+        {
+            name: 'Wayne Barnett',
+            Role: 'Founder & CEO',
+            picture: 'wayne-barnett-founder-ceo.jpg'
+        },
+        {
+            name: 'Angela Caroll',
+            Role: 'Chief Editor',
+            picture: 'angela-caroll-chief-editor.jpg'
+        },
+        {
+            name: 'Walter Gordon',
+            Role: 'Office Manager',
+            picture: 'walter-gordon-office-manager.jpg'
+        },
+        {
+            name: 'Angela Lopez',
+            Role: 'Social Media Manager',
+            picture: 'angela-lopez-social-media-manager.jpg'
+        },
+        {
+            name: 'Scott Estrada',
+            Role: 'Developer',
+            picture: 'scott-estrada-developer.jpg'
+        },
+        {
+            name: 'Barbara Ramos',
+            Role: 'Graphic Designer',
+            picture: 'barbara-ramos-graphic-designer.jpg'
+        }
+    ]
 
 }
 
